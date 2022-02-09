d188ecec155a6a2bbcd0f2517c90e1e1ba43b9dd
Kevin <kevinurm2005@gmail.com>
2022-02-09 14:09:27 +0200

    *

---
+++ Kordused_Massiivid.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SHARP_
+{
+    class Kordused_Massiivid
+    {
+        static void Main(string[] args)
+        {
+            int[] arvudd = new int[5];
+            int a=0;
+            int k=0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Sisesta arv");
+                    a = int.Parse(Console.ReadLine());
+                    arvudd[k] = a;
+                    k++;
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp);
+                    
+                }
+            } while (k<5);
+            int summa = 0;
+            int korr = 1;
+            foreach (int arv in arvudd)
+            {
+                summa += arv;
+                korr *= arv;
+            }
+            Console.WriteLine($" Summa= {summa}");
+            Console.WriteLine($" Korrutis= {korr}");
+            Console.WriteLine($" Keskmine= {summa / arvudd.Length,4}");
+
+            Console.ReadKey();
+            //Random rnd = new Random();
+            //int N = rnd.Next(1,100);
+            //int M = rnd.Next(1,100);
+
+            //Console.WriteLine(N);
+            //Console.WriteLine(M);
+
+            //if (N >= M)
+            //{
+            //    int abi = N;
+            //    N = M;
+            //    M = abi;
+            //}
+            //int[] N_M = new int[M-N+1];
+            //int j = 0;
+            //for (int i = N; i < M+1; i++)
+            //{
+            //    N_M[j] = i*i;
+            //    j++;
+            //}
+            //foreach (int n_m in N_M)
+            //{
+            //    Console.Write($"{n_m,6}");
+            //}
+
+            //char[] Tahed = new char[7] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            //Console.WriteLine($"Esimine täht on {Tahed[0]}");
+
+            //foreach (char taht in Tahed)
+            //{
+            //    Console.Write($"{taht, 2}");
+            //}
+            //Console.WriteLine();
+            //Random rnd = new Random();
+            //int[] arvud = new int[10];
+            //for (int i = 0; i < arvud.Length; i++)
+            //{
+            //    arvud[i] = rnd.Next(0, 10);
+            //}
+            //foreach (int arv in arvud)
+            //{
+            //    Console.Write($"{arv,2}");
+            //}
+            //Console.WriteLine();
+            //int[,] tabel = new int[5, 10];
+            //for (int i = 1; i < 6; i++)
+            //{
+            //    Console.Write($"rida {i}: ");
+            //    for (int ii = 1; ii < 11; ii++)
+            //    {
+            //        Console.Write($"koht {ii}");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,66 +10,76 @@ namespace C_SHARP_
     {
         static void Main(string[] args)
         {
-            //    double price = 10;
-            //    string pilet_type = "";
-            //    Console.WriteLine("Ur name?");
-            //    string eesnimi = Console.ReadLine();
-            //    Console.WriteLine("Welcome! {0}", eesnimi);
-            //    if (eesnimi.ToLower()=="dan")
-            //    {
-            //        Console.WriteLine("Come over :) Do you want to go to the cinema?");
-            //        string vaastus = Console.ReadLine();
-            //        if (vaastus.ToUpper()=="YES" || vaastus.ToUpper()=="YEP")
-            //        {
-            //            Console.WriteLine("How old are u?");
-            //            int vanus = int.Parse(Console.ReadLine());
-            //            if (vanus < 0 || vanus > 108)
-            //            {
-            //                Console.WriteLine("You are not allowed!");
-            //                pilet_type = "not allowed";
-            //            }
-            //            else if (vanus>=6 && vanus <12)
-            //            {
-            //                price -= price * 0.3;
-            //                pilet_type = "children";
-            //            }
-            //            else if (vanus >=12 && vanus<18)
-            //            {
-            //                price -= price * 0.1;
-            //                pilet_type = "default";
-            //            }
-            //            else if (vanus < 6 || vanus >= 65)
-            //            {
-            //                price -= price * 1;
-            //                pilet_type = "free";
-            //            }
-            //            eesnimi = eesnimi.ToLower();
-            //            Console.WriteLine("{0}, you'll need to pay {1} euro", UppercaseFirst(eesnimi), price,Console.Title);
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Okay :(");
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("I'm not waiting for you! :(");
-            //    }
-            //}
-            //static string UppercaseFirst(string s) //to make string's first letter upper
-            //{
-            //    if (string.IsNullOrEmpty(s))
-            //    {
-            //        return string.Empty;
-            //    }
-            //    return char.ToUpper(s[0]) + s.Substring(1);
+            ConsoleKeyInfo k;
+            do
+            {
+                Console.WriteLine("Vajuta mingi klahv!!!");
+                k = Console.ReadKey();
+                Console.Beep();
+            } while (k.Key!=ConsoleKey.Escape);
+            Environment.Exit(0);
+            double price = 10;
+            price = price / 30;
+            price = price * 100;
+            string pilet_type = "";
+            Console.WriteLine("Ur name?");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Welcome! {0}", eesnimi);
+            if (eesnimi.ToLower() == "dan")
+            {
+                Console.WriteLine("Come over :) Do you want to go to the cinema?");
+                string vaastus = Console.ReadLine();
+                if (vaastus.ToUpper() == "YES" || vaastus.ToUpper() == "YEP")
+                {
+                    Console.WriteLine("How old are u?");
+                    int vanus = int.Parse(Console.ReadLine());
+                    if (vanus < 0 || vanus > 108)
+                    {
+                        Console.WriteLine("You are not allowed!");
+                        pilet_type = "not allowed";
+                    }
+                    else if (vanus >= 6 && vanus < 12)
+                    {
+                        price -= price * 0.3;
+                        pilet_type = "children";
+                    }
+                    else if (vanus >= 12 && vanus < 18)
+                    {
+                        price -= price * 0.1;
+                        pilet_type = "default";
+                    }
+                    else if (vanus < 6 || vanus >= 65)
+                    {
+                        price -= price * 1;
+                        pilet_type = "free";
+                    }
+                    eesnimi = eesnimi.ToLower();
+                    Console.WriteLine("{0}, you'll need to pay {1} euro", UppercaseFirst(eesnimi), price, Console.Title);
+                }
+                else
+                {
+                    Console.WriteLine("Okay :(");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I'm not waiting for you! :(");
+            }
+        }
+        static string UppercaseFirst(string s) //to make string's first letter upper
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            return char.ToUpper(s[0]) + s.Substring(1);
 
             //---------------------------------------------
 
             //Console.WriteLine("Write 2 names");
             //string name1 = Console.ReadLine();
             //string name2 = Console.ReadLine();
-            //if (name1.ToLower()=="kevin" && name2.ToLower()=="german" || name1.ToLower() == "german" && name2.ToLower() == "kevin")
+            //if (name1.ToLower() == "kevin" && name2.ToLower() == "german" || name1.ToLower() == "german" && name2.ToLower() == "kevin")
             //{
             //    Console.WriteLine("yes");
             //}
@@ -96,10 +106,10 @@ namespace C_SHARP_
 
             //----------------------------------------
 
-            Console.WriteLine("Write 2 ");
+            //Console.WriteLine("Write 2 ");
 
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
